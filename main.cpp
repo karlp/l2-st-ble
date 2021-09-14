42caa1c32efe8dcf2203600170a82d6c2eeddc54
Karl Palsson <karlp@tweak.net.au>
2021-09-14 23:01:23 +0000

    Include some basic stubs from example code

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+#include <gpio/gpio.h>
+#include <usart/usart.h>
+#include <interrupt/interrupt.h>
+#include <rcc/rcc.h>
+#include <usb/usb.h>
+#include <usb/descriptor.h>
+#include <timer/timer.h>
+
+
 int main() {
+	rcc_init();
 	return 0;
 }
