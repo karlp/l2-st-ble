26ee9ec7a2daa65926340d4d725d9d4500125077
Karl Palsson <karlp@etactica.com>
2021-09-16 12:21:44 +0000

    freertos: turn off legacy renaming compat crap

--- FreeRTOSConfig.h
+++ FreeRTOSConfig.h
@@ -5,6 +5,8 @@
  * You _should_ probably re-read https://www.freertos.org/a00110.html !
  */
 
+#define configENABLE_BACKWARD_COMPATIBILITY	0
+
 #define configUSE_PREEMPTION                    1
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION 1
 #define configUSE_TICKLESS_IDLE                 1
