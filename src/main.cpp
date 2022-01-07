2628a86e5449b165bb3b3d56b98460f011737c81
Karl Palsson <karlp@tweak.net.au>
2022-01-07 00:12:02 +0000

    hacking forwards

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,18 @@
 #include <cal/cal.h>
 #include <cortex_m/debug.h>
 #include <dma/dma.h>
+#include <exti/exti.h>
 #include <gpio/gpio.h>
 #include <interrupt/interrupt.h>
+#include <pwr/pwr.h>
 #include <rcc/flash.h>
 #include <rcc/rcc.h>
 #include <timer/timer.h>
 #include <uart/uart.h>
 #include <usb/usb.h>
 #include <usb/descriptor.h>
+#include <wpan/hsem.h>
+#include <wpan/ipcc.h>
 
 #include "FreeRTOS.h"
 #include "task.h"
@@ -27,6 +31,8 @@ auto led_b = GPIOB[5];
 
 #define SAVE_TO_SECOND_BUFFER
 
+// This makes it more ~equivalent to the ST demos, that all run at 32MHz...
+#define RUNNING_AT_32MHZ
 
 #if defined(RUNNING_AT_32MHZ)
 /**
@@ -40,6 +46,10 @@ void krcc_init32(void) {
 	RCC->CR |= (1<<16);
 	while(!(RCC->CR & (1<<17)));
 
+	/* I initially setup PLL to 32Mhz, because I had PLL to 64, and just downtuned
+	 * but if you want lower power, use HSE by itself at 32.  The PLL is useless burn
+	 */
+#if defined(USE_PLL_32)
 	// Configure and enable PLL.
 	// R=2, Q = 2, P = 2, M = 2, N = 8, src=HSE
 	const auto m = 2;
@@ -56,8 +66,80 @@ void krcc_init32(void) {
 	// Switch to PLL.
 	RCC->CFGR |= 0x3;
 	while((RCC->CFGR & (3 << 2)) != (3 << 2)); // SWS = PLL
-
+#else
+	RCC->CFGR |= 0x2;
+	while ((RCC->CCIPR & (2<<2)) != (2<<2)); // SWS = HSE
+#endif
 	// Leave prescalers alone...
+	
+	PWR->CR1 |= (1<<8); // Unlock backup domain
+	RCC->BDCR |= (1<<0); // LSE ON
+
+	/////// FROM HERE DOWN IS PROBABLY NOT PART OF THE "rcc32_init" step? ///////////////
+	// Set SMPS clock to HSE.  (This is _ignored_ if the radio is active, and HSE is used regardless!)
+	RCC->SMPSCR = (1<<4) | (2<<0); // HSE+div1.
+	
+	// Set RF wakeup clock to LSE
+	RCC->CSR |= (1<<14);
+	
+	// MX_IPCC_Init()...
+	RCC.enable(rcc::IPCC);
+	NVIC.enable(interrupt::irq::IPCC_C1_RX);
+	NVIC.enable(interrupt::irq::IPCC_C1_TX);
+	// XXX: IPCC reset? yolo! I don't see what state we should bother with this in...
+//	IPCC->C1CR = 0;
+//	IPCC->C1MR = 0x3f << 16 | 0x3f;
+//	IPCC->C1SCR = 0x3f;
+	// Ok, but seriously,
+	IPCC->C1CR |= (1<<16) | (1<<0); // TXFIE | RXOIE
+	
+	// MX_RF_Init() is null
+	// MX_RTC_Init()...
+	// FIXME: XXX fuck it, I'm not sure what we're using this for, it can wait...
+	// demo app seems to be using it for a wakeup timer? I'll have adc dma timer for that...
+	
+	
+	// MX_APPE_Init()...
+	// CAN this go to top of bluetooth? (appears to still do exti/smps shits..
+	PWR->CR5 &= ~(7 << 4); // 80mA startup current
+	// Not sure why we need to do this, but... It talks about limiting rf output power?
+	int32_t now = PWR->CR5 & 0x7; // reset calibration is at 1.5V
+	now -= 2; // Attempt to get 1.4V
+	if (now > 0 && now < 7) {
+		PWR->CR5 |= now;
+	} else {
+		printf("yolo smps setting?!");
+	}
+	
+	EXTI->IMR2 |= (1 << (36-32)) | (1<<(38-32)); // IPCC and HSEM wakeup EXTIs
+	
+	// XXX: more RTC init here, setting wakeup clocks.
+	
+	// SystemPower_Config()....
+	// set hsi as sysclock after wakeup from stop?
+	// ->  nope, we're never going to stop...
+	// init "util_lpm_..." -> nope...
+	PWR->C2CR1 &= ~(0x7);
+	PWR->C2CR1 |= 0x4; // LPMS == Shutdown
+	
+	
+	// HW_TS_Init()....
+	// FIXME - this one _might_ need the RTC finally?
+	
+	///// APPD_Init()
+	// XXX: there's a step here about enabling debugger, which is a power thing... revisit.
+	
+	// APPD_SetCPU2GpioConfig() ? lol, no, we're not getting support from ST with this code :)
+	// APPD_bleDtbCfg() ? lol, same, no ST support here bois!
+	/////
+	
+	// UTIL_LPM modes again.  we're definitely not going to be using that code, we want it better tied into freertos..
+	
+	// appe_Tl_Init()...
+	// XXX this is a real good sized one!
+	// go straight to another file for it...
+	ble_tl_init();
+	
 }
 #endif
 
@@ -79,6 +161,8 @@ static void prvTaskBlinkGreen(void *pvParameters)
 
 
 int main() {
+	ble_pre();
+	
 #if defined(RUNNING_AT_32MHZ)
 	krcc_init32();
 #else
--- src/t_ble.cpp
+++ src/t_ble.cpp
@@ -30,6 +30,45 @@ struct ts_ble_t task_state_ble;
 
 
 
+//(gdb) x /64x 0x1FFF7000
+//0x1fff7000:	0xe1051355	0x00170080
+//(gdb) x /64b 0x1FFF7000
+//0x1fff7000:	0x55	0x13	0x05	0xe1	0x80	0x00	0x17	0x00
+
+
+//  typedef  PACKED_STRUCT
+//  {
+//    uint8_t   bd_address[6];
+//    uint8_t   hse_tuning;
+//    uint8_t   id;
+//  } OTP_ID0_t;
+
+/* WARNING MUST BE CALLED WITH HSE OFF! */
+static void _rcc_tune_hse(uint16_t tune) {
+	RCC->HSECR = 0xcafecafe; // unlock
+	RCC->HSECR |= (tune & 0x3f) << 8;
+}
+
+static void _tune_hse(void) {
+	// FIXME - this is wrong, they apparently re-write the struct downwards,
+	// and have a function to scan backwards looking for a valid block...
+	if (STM32::Calibration::OTP_ID0.hse_tuning) {
+		
+	}
+	// on my board, it has one entry, and "id 0 therefore implies hse tuning of 0x17...
+	// and this is what the ST demo app does.
+	_rcc_tune_hse(0x17);
+}
+
+void ble_pre(void) {
+	// ST demos reset IPCC and backup domain here, but.... let's not, and get bitten by that later.	
+	_tune_hse();
+}
+
+void ble_tl_init(void) {
+	
+}
+
 void task_ble(void *pvParameters)
 {
 	struct ts_ble_t *ts = (struct ts_ble_t*)pvParameters;
