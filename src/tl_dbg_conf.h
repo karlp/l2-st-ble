ea54c65041260763bd57c51b3c210e4cdeb3882a
Karl Palsson <karlp@tweak.net.au>
2022-01-25 22:10:26 +0000

    tl_dbg_conf: allow turning off bumper dumping
    it can be needlessly verbose

--- src/tl_dbg_conf.h
+++ src/tl_dbg_conf.h
@@ -7,21 +7,31 @@ void DbgTraceBuffer( const void *pBuffer , uint32_t u32Length , const char *strF
 #define USE_NULL 0
 #define USE_STDIO 1
 
+// Buffers can be pretty verbose, don't bother including them normally.
+#define DEBUG_BUFS 0
+
 #if USE_STDIO
 /* Let stdio re-targetting handle this directly... ? */
 #define TL_MM_DBG_MSG(...)	fprintf(stderr, __VA_ARGS__)
 #define TL_HCI_CMD_DBG_MSG(...) fprintf(stderr, __VA_ARGS__)
-#define TL_HCI_CMD_DBG_BUF(...) DbgTraceBuffer(__VA_ARGS__)
 #define TL_HCI_CMD_DBG_RAW(...)
 #define TL_HCI_EVT_DBG_MSG(...) fprintf(stderr, __VA_ARGS__)
-#define TL_HCI_EVT_DBG_BUF(...) DbgTraceBuffer(__VA_ARGS__)
 #define TL_HCI_EVT_DBG_RAW(...)
 #define TL_SHCI_CMD_DBG_MSG(...) fprintf(stderr, __VA_ARGS__)
-#define TL_SHCI_CMD_DBG_BUF(...) DbgTraceBuffer(__VA_ARGS__)
 #define TL_SHCI_CMD_DBG_RAW(...)
 #define TL_SHCI_EVT_DBG_MSG(...) fprintf(stderr, __VA_ARGS__)
-#define TL_SHCI_EVT_DBG_BUF(...) DbgTraceBuffer(__VA_ARGS__)
 #define TL_SHCI_EVT_DBG_RAW(...)
+#if (DEBUG_BUFS == 1)
+#define TL_HCI_CMD_DBG_BUF(...) DbgTraceBuffer(__VA_ARGS__)
+#define TL_HCI_EVT_DBG_BUF(...) DbgTraceBuffer(__VA_ARGS__)
+#define TL_SHCI_CMD_DBG_BUF(...) DbgTraceBuffer(__VA_ARGS__)
+#define TL_SHCI_EVT_DBG_BUF(...) DbgTraceBuffer(__VA_ARGS__)
+#else
+#define TL_HCI_CMD_DBG_BUF(...)
+#define TL_HCI_EVT_DBG_BUF(...)
+#define TL_SHCI_CMD_DBG_BUF(...)
+#define TL_SHCI_EVT_DBG_BUF(...)
+#endif
 #endif
 
 #if USE_NULL
