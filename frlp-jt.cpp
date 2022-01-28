9b99d34b63ed36e09a76eadb34ef59a39570d419
Karl Palsson <karlp@tweak.net.au>
2022-01-28 09:09:48 +0000

    frlp-jt: simplify headers and rename
    no need for both license text and spdx identifier, lose the cruft.

--- frlp-jt.cpp
+++ frlp-jt.cpp
@@ -1,32 +1,9 @@
-// lptimTick.c -- Jeff Tenney
-//
+// SPDX-License-Identifier: MIT
 // STM32 No-Drift FreeRTOS Tick/Tickless via LPTIM
 //
 // Example integration and validation: https://github.com/jefftenney/LPTIM-Tick
 //
-// Revision: 2021.11.23
-// Tabs: None
-// Columns: 110
-// Compiler: gcc (GNU) / armcc (Arm-Keil) / iccarm (IAR)
-// SPDX-License-Identifier: MIT
-
-
 // Copyright 2021 Jeff Tenney <jeff.tenney@gmail.com>
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-// associated documentation files (the "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
-// following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all copies or substantial
-// portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
-// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
-// EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
-// USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
 #include "FreeRTOS.h"
